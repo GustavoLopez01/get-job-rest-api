46801c99bcb7c990c7b4744acb8af6ab32688742
Gustavo <gustavoalzarate@gmail.com>
2024-10-27 16:45:40 -0600

    Add functionality and validations to endpoint jobs

--- src/handlers/Job.ts
+++ src/handlers/Job.ts
@@ -1,14 +1,30 @@
 import { Request, Response} from 'express'
+import Job from '../models/Job.model';
 
 export const getAllJobs = async (req: Request, res: Response) => {
     try {
+        const jobs = await Job.findAll()
+        if(Array.isArray(jobs)) {
+            res.status(200).json(jobs)
+            return 
+        }
+
+        res.status(404).json([])
     } catch (error) {
         console.log(error);
     }
 }
 
-export const getJob = async () => {
+export const getJobById = async (req: Request, res: Response) => {
     try {
+        const { id } = req.params
+        const job = await Job.findByPk(id)
+
+        if(job) {
+            res.status(200).json(job)
+            return
+        }
+        res.status(404).json({ message: `No exists job with id ${id}` })
     } catch (error) {
         console.log(error);
     }
@@ -16,7 +32,11 @@ export const getJob = async () => {
 
 export const saveJob = async (req: Request, res: Response) => {
     try {
-        console.log(req.body);        
+        const job = await Job.create(req.body)
+        if(job) {
+            res.status(200).json(job)
+            return
+        }
         res.json(req.body)
     } catch (error) {
         console.log(error);
@@ -25,8 +45,22 @@ export const saveJob = async (req: Request, res: Response) => {
 
 export const updateJob = async (req: Request, res: Response) => {
     try {
-        console.log(req.body);        
-        res.json(req.body)
+        const id = req.body.id;
+        const existJob = await Job.findByPk(id)
+
+        if(existJob) {
+            const [updatedCount] = await Job.update({ ...req.body },
+            { where: { id } })
+
+            if(updatedCount > 0) {
+                res.status(200).json(req.body)
+                return
+            }
+        }
+
+        res.json({
+            message: `No exists Job with id ${id}`
+        })
     } catch (error) {
         console.log(error);
     }
--- src/router/jobRouter.ts
+++ src/router/jobRouter.ts
@@ -1,10 +1,34 @@
 
 import { Router } from 'express'
-import { getAllJobs, saveJob } from '../handlers/Job'
+import { body, param } from 'express-validator'
+import { getAllJobs, getJobById, saveJob, updateJob } from '../handlers/Job'
+import { validateFields } from '../middlewares/validateFields'
 
 const router = Router()
 
 router.get('/', getAllJobs)
-router.post('/', saveJob)
+
+router.get('/:id',
+    param('id')
+        .isNumeric()
+        .withMessage('Id should be a number'),
+    validateFields,
+    getJobById
+)
+
+router.post('/',
+    body('name').notEmpty(),
+    body('description').notEmpty(),
+    validateFields,
+    saveJob
+)
+
+router.put('/',
+    body('name').notEmpty(),
+    body('description').notEmpty(),
+    body('active').notEmpty(),
+    validateFields,
+    updateJob
+)
 
 export default router
